e3b5a8b8f6d7af3cea1674056ff5b1fab75feda8
Fenriz1349 <julien.cotte@live.fr>
2025-12-12 15:57:20 +0100

    first commit

---
+++ joiefull/joiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  joiefullApp.swift
+//  joiefull
+//
+//  Created by Julien Cotte on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct joiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
